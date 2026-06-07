df14955674230f42203d0971113cba8c783d21fd
Asmit Kaushal <asmitkaushal47@gmail.com>
2026-06-07 20:56:45 +0530

    changed the model which supports streaming

--- backend_chatbot.py
+++ backend_chatbot.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.memory import InMemorySaver
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
 
@@ -14,7 +14,8 @@ if not os.getenv("NVIDIA_API_KEY"):
     raise ValueError("NVIDIA_API_KEY is not set. Check your .env file!")
 
 
-llm = ChatNVIDIA(model="nvidia/nemotron-3-super-120b-a12b")
+llm = ChatNVIDIA(model="meta/llama-3.1-8b-instruct")
+
 
 
 class state(TypedDict):
@@ -26,7 +27,7 @@ from langchain_core.runnables import RunnableLambda
 chat_node = RunnableLambda(lambda state: state["messages"]) | llm | (lambda response: {"messages": response})
 
 
-checkpointer = MemorySaver()
+checkpointer = InMemorySaver()
 graph = StateGraph(state)
 graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
